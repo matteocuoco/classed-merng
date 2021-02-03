1cb4c82c5f3a57d8689a525c58c6ea2605fc27bb
matteo.cuoco.staff <matteo.cuoco.staff@gmail.com>
2021-02-03 23:35:33 +0100

    Finishing server

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
 
@@ -40,6 +40,10 @@ module.exports = {
 
       const post = await newPost.save();
 
+      context.pubsub.publish("NEW_POST", {
+        newPost: post
+      });
+
       return post;
     },
     async deletePost(_, { postId }, context) {
@@ -47,7 +51,7 @@ module.exports = {
 
       try {
         const post = await Post.findById(postId);
-        if ((user.username === post.username)) {
+        if (user.username === post.username) {
           await post.delete();
           return "Post deleted successfully";
         } else {
@@ -56,6 +60,30 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // Post already liked, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          // Posto not liked, like it
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
+    },
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST")
     },
   },
 };
