d8ad9a5c3fd3ef27fb8f81983449700ea8e76abd
matteo.cuoco.staff <matteo.cuoco.staff@gmail.com>
2021-02-04 23:12:41 +0100

    Client start

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
 const { AuthenticationError, UserInputError } = require("apollo-server");
+const { argsToArgsConfig } = require("graphql/type/definition");
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
 
@@ -29,7 +30,9 @@ module.exports = {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
 
-      console.log(user);
+      if (args.body.trim() === "") {
+        throw new Error("Post body must not be empty");
+      }
 
       const newPost = new Post({
         body,
@@ -41,7 +44,7 @@ module.exports = {
       const post = await newPost.save();
 
       context.pubsub.publish("NEW_POST", {
-        newPost: post
+        newPost: post,
       });
 
       return post;
@@ -83,7 +86,7 @@ module.exports = {
   },
   Subscription: {
     newPost: {
-      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST")
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST"),
     },
   },
 };
